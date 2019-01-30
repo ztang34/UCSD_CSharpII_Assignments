7f652649867606fe45adff0f6dade582dc0658dd
Zhenguan Tang <ztang@osisoft.com>
2019-01-29 22:03:32 -0800

    Added Untils Class and Point Class

---
+++ Lab2/Lab2/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public struct Point
+    {
+        public double X, Y;
+
+        public Point (double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Distance (Point other)
+        {
+            return Math.Sqrt(Math.Pow((this.X - other.X), 2) + Math.Pow((this.Y - other.Y), 2)); //Calculate distance between two points in Cartessian Coordinates
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Lab2/Lab2/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public static class Utils
+    {
+        public static bool IsRelativelyEqual (double d1, double d2)
+        {
+            double margin = Math.Abs((d1 + d2) * 0.0001 / 2);
+            double diff = Math.Abs(d1 - d2);
+
+            if (diff < margin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static Tuple<double, double, double, double> GetBoundingBox(List<Point> points)
+        {
+            double minX = points.Min(point => point.X);
+            double maxX = points.Max(point => point.X);
+            double minY = points.Min(point => point.Y);
+            double maxY = points.Max(point => point.Y);
+
+            return new Tuple<double, double, double, double>(minX, minY, maxX, maxY);
+        }
+    }
+}
