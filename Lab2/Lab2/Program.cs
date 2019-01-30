256861ba2d804e2f42cda7ed60f371ee41b5c747
Zhenguan Tang <ztang@osisoft.com>
2019-01-29 21:33:02 -0800

    Added Lab 2 into repo

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press <ENTER> to continue...");
+            Console.ReadLine();
+        }
+    }
+}
