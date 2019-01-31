e5007f085519498c039412814a0bacb1dd3af884
Zhenguan Tang <ztang@osisoft.com>
2019-01-30 19:33:04 -0800

    Completed Triangle class

--- Lab2/Lab2/Triangle.cs
+++ Lab2/Lab2/Triangle.cs
@@ -28,6 +28,59 @@ namespace Lab2
             }
         }
 
+        private double[] SideLength //returns array with triangle side lengths
+        {
+            get
+            {
+                double[] sideLength = new double[3];
+                sideLength[0] = Vertices[0].Distance(Vertices[1]);
+                sideLength[1] = Vertices[1].Distance(Vertices[2]);
+                sideLength[2] = Vertices[2].Distance(Vertices[0]);
+                Array.Sort(sideLength);
+
+                return sideLength;
+
+            }
+        }
+
+        public Triangle(List<Point> vertices) : base (vertices)
+        {
+
+        }
+
+        public Triangle(Point v1, Point v2, Point v3) : base (new List<Point>() { v1, v2,v3})
+        {
+
+        }
+
+        public override double Area()
+        {
+            double s = Perimeter() / 2;
+            double area = Math.Sqrt(s * (s - SideLength[0]) * (s - SideLength[1]) * (s - SideLength[2]));
+
+            return area;
+        }
+
+        public virtual bool IsEquilateral()
+        {
+            return (Utils.IsRelativelyEqual(SideLength[0], SideLength[2]));
+        }
+
+        public virtual bool IsIsosceles()
+        {
+            return (Utils.IsRelativelyEqual(SideLength[0], SideLength[1]) || Utils.IsRelativelyEqual(SideLength[1], SideLength[2]));
+        }
+
+        public virtual bool IsScalene()
+        {
+            return !IsIsosceles();
+        }
+
+        public override string ToString()
+        {
+            return ("Triangle: " + base.ToString());
+        }
+
 
 
     }
