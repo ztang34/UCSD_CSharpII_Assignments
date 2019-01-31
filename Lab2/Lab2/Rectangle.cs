a7633ba127792c11b6123783470101f83f58bdf0
Zhenguan Tang <ztang@osisoft.com>
2019-01-30 23:06:00 -0800

    Completed Rectangle Class

---
+++ Lab2/Lab2/Rectangle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Rectangle : Shape
+    {
+        public override List<Point> Vertices
+        {
+            get
+            {
+                return base.Vertices;
+            }
+            set
+            {
+                if (value.Count == 2 || value.Count == 4)
+                {
+                    Vertices = Normalize(value);
+                }
+                else
+                {
+                    throw new ArgumentException("Vertices", "Rectangle can only have two or 4 vertices");
+                }
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                var bounds = Utils.GetBoundingBox(Vertices);
+                return (bounds.Item3 - bounds.Item1);
+            }
+
+        }
+
+        public double Height
+        {
+            get
+            {
+                var bounds = Utils.GetBoundingBox(Vertices);
+                return (bounds.Item4 - bounds.Item2);
+            }
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+
+        public bool IsSquare()
+        {
+            return (Height == Width);
+        }
+
+        public List<Triangle> ToTriangles()
+        {
+            List<Triangle> triangles = new List<Triangle>(2);
+            triangles.Add(new Triangle(Vertices[0], Vertices[1], Vertices[2]));
+            triangles.Add(new Triangle(Vertices[1], Vertices[2], Vertices[4]));
+
+            return triangles;
+        }
+
+        public override string ToString()
+        {
+            return "Rectangle: "+  base.ToString();
+        }
+
+        private List<Point> Normalize (List<Point> input)
+        {          
+          var bounds = Utils.GetBoundingBox(input);
+
+          Point pt1 = new Point(bounds.Item1, bounds.Item2);
+          Point pt2 = new Point(bounds.Item1, bounds.Item4);
+          Point pt3 = new Point(bounds.Item3, bounds.Item4);
+          Point pt4 = new Point(bounds.Item3, bounds.Item2);
+
+          return new List<Point>() { pt1, pt2, pt3, pt4 };      
+        }
+
+
+    }
+}
