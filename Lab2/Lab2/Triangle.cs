18d977c27a2c0981d254f91175fb1c18bcdf3ba3
Zhenguan Tang <ztang@osisoft.com>
2019-01-30 16:31:20 -0800

    Added Triangle Class

---
+++ Lab2/Lab2/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Triangle : Shape
+    {
+        public override List<Point> Vertices
+        {
+            get
+            {
+                return base.Vertices;
+            }
+            set
+            {
+                if (Vertices.Count != 3)
+                {
+                    throw new ArgumentException("Vertices", "Triangle can only have 3 vertices!");
+                }
+                else
+                {
+                    Vertices = value;
+                }
+
+            }
+        }
+
+
+
+    }
+}
