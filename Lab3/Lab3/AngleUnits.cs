80867cb45b5ddc8475fddaf41eac6da81d054ef4
Zhenguan Tang <ztang@osisoft.com>
2019-02-03 12:30:23 -0800

    Started Lab3 Projects

---
+++ Lab3/Lab3/AngleUnits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public enum AngleUnits
+    {
+        Degrees = 0,
+        Gradians,
+        Radians,
+        Turns
+    }
+}
