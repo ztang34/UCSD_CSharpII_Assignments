97d412a3952e5266f2c1976839d653e4304adc60
Zhenguan Tang <ztang@osisoft.com>
2019-02-05 22:08:46 -0800

    Created AngleFormatter class

---
+++ Lab3/Lab3/AngleFormatter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class AngleFormatter : ICustomFormatter, IFormatProvider
+    {
+        public const int DefaultDecimalPlace = -1;
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null)
+            {
+                throw new ArgumentNullException("Variable is not set to an instance of object!");            
+            }
+            else if (arg is Angle)
+            {
+                Angle a = arg as Angle;
+                return string.Empty;
+            }
+            else if (arg is IFormattable)
+            {
+                return ((IFormattable)arg).ToString(format, formatProvider);
+            }
+            else
+            {
+                return arg.ToString();
+            }
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (typeof(ICustomFormatter).Equals(formatType))
+            {
+                return this;
+            }
+            return null;
+        }
+
+        public int GetDecimalPlace(string format)
+        {
+            if(format.Length < 2 || !char.IsDigit(format[1]))
+            {
+                return DefaultDecimalPlace;
+            }
+            else
+            {
+                return int.Parse(format[1].ToString());
+            }
+        }
+        
+    }
+}
