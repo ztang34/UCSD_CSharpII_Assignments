971b1a41791c6509c3dba4f1b3b44d6fd5d3633d
Zhenguan Tang <ztang@osisoft.com>
2019-02-06 16:58:01 -0800

    Completed AngleFormatter class

--- Lab3/Lab3/AngleFormatter.cs
+++ Lab3/Lab3/AngleFormatter.cs
@@ -18,7 +18,43 @@ namespace Lab3
             else if (arg is Angle)
             {
                 Angle a = arg as Angle;
-                return string.Empty;
+                decimal value;
+                
+                if(String.IsNullOrWhiteSpace(format) || string.IsNullOrEmpty(format))
+                {
+                    return a.Value.ToString("f2") + a.Units.ToSymbol();
+                }
+                else
+                {
+                    char unit = char.ToLower(format[0]); //ignore case
+                    string decimalPlace = GetDecimalPlace(format,unit); 
+
+                    switch (unit)
+                    {
+                        case 'c':
+                            value = a.Value; 
+                            return value.ToString(decimalPlace) + a.Units.ToSymbol();
+                        case 'd':
+                            value = a.ToDegrees().Value;
+                            return value.ToString(decimalPlace) + AngleUnits.Degrees.ToSymbol();
+                        case 'g':
+                            value = a.ToGradians().Value;
+                            return value.ToString(decimalPlace) + AngleUnits.Degrees.ToSymbol();
+                        case 'p':
+                            value = a.ToRadians().Value / (decimal)Math.PI;
+                            return value.ToString(decimalPlace) + "π" + AngleUnits.Radians.ToSymbol();
+                        case 'r':
+                            value = a.ToRadians().Value;
+                            return value.ToString(decimalPlace) + AngleUnits.Radians.ToSymbol();
+                        case 't':
+                            value = a.ToTurns().Value;
+                            return value.ToString(decimalPlace) + AngleUnits.Turns.ToSymbol();
+                        default:
+                            value = a.Value;
+                            return value.ToString(decimalPlace) + a.Units.ToSymbol();
+                    }
+                }
+               
             }
             else if (arg is IFormattable)
             {
@@ -39,15 +75,27 @@ namespace Lab3
             return null;
         }
 
-        public int GetDecimalPlace(string format)
+        public string GetDecimalPlace(string format, char unit)
         {
-            if(format.Length < 2 || !char.IsDigit(format[1]))
+            if(String.IsNullOrEmpty(format) || String.IsNullOrWhiteSpace(format) || !char.IsDigit(format[1])) //invalid format code
             {
-                return DefaultDecimalPlace;
+                return "f2";
+            }
+            else if (format.Length < 2) //not specifying decimal place
+            {
+                if (unit == 'p' || unit == 'r')
+                {
+                    return "f5"; //default decimal place for radian
+                }
+                else
+                {
+                    return "f2"; //default decimal place for all other units
+                }
+
             }
             else
             {
-                return int.Parse(format[1].ToString());
+                return "f" + format[1].ToString();
             }
         }
         
