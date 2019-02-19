a6e0f436c70141d7d7fa3d805a9eb5a84965bce0
Zhenguan Tang <ztang@osisoft.com>
2019-02-18 21:35:35 -0800

    Starts this homework

---
+++ Lab5/Lab5/DocumentStatistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Lab5
+{
+    [DataContract]
+    class DocumentStatistics
+    {
+        [DataMember]
+        public int DocumentCount { get; set; }
+        [DataMember]
+        public List<string> Documents { get; set; }
+        [DataMember]
+        public Dictionary<String, int> WordCounts { get; set; }
+
+        public DocumentStatistics()
+        {
+            DocumentCount = 0;
+            Documents = new List<String>();
+            WordCounts = new Dictionary<string, int>();
+        }
+    }
+}
---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void ProcessFiles(string filepath, DocumentStatistics stats)
+        {
+
+        }
+
+        private static void SerializeStats(string filepath, DocumentStatistics stats)
+        {
+
+        }
+    }
+}
